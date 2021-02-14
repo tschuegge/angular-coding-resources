5956bc22b87c49451f410a02ce83585f4edc1cf5
Jürg Danuser <juerg.danuser@bzbuchs.ch>
2021-02-14 14:24:16 +0100

    Aktualisierung auf Angular 11

--- projects/angular-coding-resources/src/lib/colored-circle/colored-circle.component.ts
+++ projects/angular-coding-resources/src/lib/colored-circle/colored-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Zeigt einen Kreis an
@@ -14,7 +14,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ColoredCircleComponent {
 
   /**
-   * CSS kompatibler Farbname: https://developer.mozilla.org/de/docs/Web/CSS/Farben#Farbschlüsselwörter
+   * CSS kompatibler Farbname: https://developer.mozilla.org/de/docs/Web/CSS/Farben#farbschlüsselwörter
    */
   @Input()
   color = 'lightgray';
